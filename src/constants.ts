2925d1d811a34215f793b31cc6f5b44ef6f1949f
JimmyRouter <19bdp88@gmail.com>
2025-03-02 13:13:19 +0000

    env_api3

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const MY_API_ENDPOINT: string = process.env.REACT_APP_API_URL || "https://zone-t.ru/api";
+export const MY_API_ENDPOINT: string = process.env.REACT_APP_API_URL || "https://zone-t.ru/";
